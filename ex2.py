593913175b233ca6cff956886dcf8db0c2118173
kotek <aviv.kotek@cs.huji.ac.il>
2018-12-06 13:25:55 +0200

    Initial commit

---
+++ ex2.py
@@ -0,0 +1,59 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import datasets
+
+# Constants
+# -------------
+AMOUNT = 300
+# -------------
+
+
+def gen_uniform_dist(low, high):
+    # Returns a np.array([...]) of uniformly 2D
+    # distributed elements from [low] to [high]:
+    x = np.random.uniform(low[0], low[1], AMOUNT)
+    y = np.random.uniform(high[0], high[1], AMOUNT)
+    return x, y
+
+
+def gen_gaussian_dist(mu, sigma):
+    pts_arr = np.random.multivariate_normal(mu, sigma, AMOUNT)  # [[x, y]] 2d np.array
+    return pts_arr[:, 0], pts_arr[:, 1]
+
+
+def gen_circle_inside_ring():
+    datasets.make_circles()
+
+
+def gen_first_letters():
+    # Generates 2d points that represent
+    # letters of AK (Artiyum and Kotek):
+    pass
+
+
+def plot(x, y):
+    plt.figure()
+    plt.plot(x, y, 'o')
+    plt.show()
+
+
+def q3():
+    # -------------------
+    # 3.a:
+    x1, y1 = gen_uniform_dist([-1, 1], [0, 5])
+    # plot(x1, y1)
+    # -------------------
+    # 3.b
+    x2, y2 = gen_gaussian_dist(([1, 1]), np.array([[4, 0], [0, 4]]))  # COV is diagonal (4,0,0,4)
+    plot(x2, y2)
+    # -------------------
+    # 3.c
+    # np.array of [ [x1],[y1], [x2],[y2], .... ]
+    # we'll merge even/odd rows and get merged X and merged Y arrays and plot:
+    gauss_merged = np.concatenate([gen_gaussian_dist(([i, -i]), ([[2 * i, 0], [0, 2 * i]])) for i in range(1, 4)])
+    x3, y3 = np.concatenate(gauss_merged[::2]), np.concatenate(gauss_merged[1::2,:])
+    plot(x3, y3)
+    # -------------------
+    # 3.d
+
+q3()
